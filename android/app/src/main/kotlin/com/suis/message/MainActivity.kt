4e6f0ff228f6f08d6745bf8fb373697ee3771131
Hope Kagame <hopekagame1@gmail.com>
2023-07-21 23:16:00 +0300

    Bootstrap app

---
+++ android/app/src/main/kotlin/com/suis/message/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.suis.message.suis_message
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:suis_message/app/app.dart';
+import 'package:suis_message/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
