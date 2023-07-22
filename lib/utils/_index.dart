c2795114fa03f009d32208cfa4530bb0402e3ae9
Hope Kagame <hopekagame1@gmail.com>
2023-07-22 22:50:12 +0300

    Add app theme

--- lib/utils/_index.dart
+++ lib/utils/_index.dart
@@ -1,6 +1,8 @@
 library sm_utils;
 
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:suis_message/counter/counter.dart';
 
 part 'sm_router.dart';
+part 'sm_theme.dart';
---
+++ lib/utils/sm_theme.dart
@@ -0,0 +1,9 @@
+part of '_index.dart';
+
+class SuisMessageTheme {
+  static ThemeData get theme => _theme;
+
+  static final _theme = ThemeData(
+    useMaterial3: true,
+  );
+}
