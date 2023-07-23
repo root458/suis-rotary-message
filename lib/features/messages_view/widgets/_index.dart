e16d4a0e7db0147d7dac983b35155629bf0bc819
Hope Kagame <hopekagame1@gmail.com>
2023-07-23 23:02:42 +0300

    Update theme

---
+++ lib/features/messages_view/widgets/_index.dart
@@ -0,0 +1,1 @@
+export 'message_view_appbar.dart';
--- lib/utils/sm_theme.dart
+++ lib/utils/sm_theme.dart
@@ -4,6 +4,7 @@ class SuisMessageTheme {
   static ThemeData get theme => _theme;
 
   static final _theme = ThemeData(
+    brightness: Brightness.dark,
     fontFamily: 'RobotoRegular',
     useMaterial3: true,
   );
