4a53a94b0888a7ba010e3279996fc4894be02dd5
Hope Kagame <hopekagame1@gmail.com>
2023-07-23 23:30:52 +0300

    Update chat item view

--- lib/features/messages_view/widgets/message_view_chat_item.dart
+++ lib/features/messages_view/widgets/message_view_chat_item.dart
@@ -6,28 +6,38 @@ class MessageViewChatItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      leading: CircleAvatar(
-        radius: 30,
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        child: Text(
-          'H',
-          style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                color: Theme.of(context).colorScheme.primaryContainer,
-                fontSize: 25,
-              ),
+      onTap: () {},
+      leading: Transform.translate(
+        offset: const Offset(0, -15),
+        child: CircleAvatar(
+          radius: 30,
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          child: Text(
+            'H',
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primaryContainer,
+                  fontSize: 25,
+                ),
+          ),
         ),
       ),
       title: Text(
         'Hassan',
         style: Theme.of(context).textTheme.titleLarge,
+        overflow: TextOverflow.ellipsis,
       ),
       subtitle: Text(
-        'Hello',
+        '''Hello there! How are you? I am going to the mall. Do you want to come with me?''',
         style: Theme.of(context).textTheme.bodyLarge,
+        overflow: TextOverflow.ellipsis,
+        maxLines: 3,
       ),
-      trailing: Text(
-        '12:00',
-        style: Theme.of(context).textTheme.bodyLarge,
+      trailing: Transform.translate(
+        offset: const Offset(10, -35),
+        child: Text(
+          '12:00',
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
       ),
     );
   }
